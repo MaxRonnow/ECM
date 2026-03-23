69760f8a0b36eef2b3fa4190fe14b6337b55ba30
Max Rönnow <max@Maxs-MacBook-Air.local>
2026-03-23 16:17:55 +0200

    Working implementation of lenstras factorisation algorithm

---
+++ src/lenstra.rs
@@ -0,0 +1,73 @@
+use num_integer::gcd;
+use rand::RngExt;
+
+pub fn ecm(n: i64) -> (i64, i64) {
+    let mut rng = rand::rng();
+
+    loop {
+        let cap_a: i64 = rng.random::<i64>() % n;
+        let a: i64 = rng.random::<i64>() % n;
+        let b: i64 = rng.random::<i64>() % n;
+
+        let cap_p = (a, b);
+        // let cap_b = (b * b - a * a * a - cap_a * a) % n;
+
+        // E: y^2 = (x^3 + Ax + B) mod n
+
+        let mut iter = 2;
+        let mut d: i64 = 0;
+        let mut div_failed = false;
+        let mut prev_point = cap_p;
+
+        while !div_failed {
+            let mut temp_point = prev_point;
+            let mut dividend: i64;
+            let mut divisor: i64;
+            for _ in 1..iter {
+                let alpha: i64;
+                if prev_point == temp_point {
+                    dividend = 3 * temp_point.0 * temp_point.0 + cap_a;
+                    divisor = 2 * temp_point.1;
+                } else {
+                    dividend = prev_point.1 - temp_point.1;
+                    divisor = prev_point.0 - temp_point.0;
+                }
+                if gcd(divisor, n) == 1 {
+                    alpha = (dividend * mod_inverse(divisor, n)) % n;
+                    let x3 = alpha * alpha - temp_point.0 - prev_point.0;
+                    temp_point = (x3, (alpha * (temp_point.0 - x3) - temp_point.1));
+                } else {
+                    div_failed = true;
+                    d = divisor;
+                }
+            }
+            prev_point = temp_point;
+            iter += iter;
+        }
+        let k = gcd(d, n);
+        if d == k && d == n {
+        } else if 1 < k && k < n {
+            return (k, n / k);
+        }
+    }
+}
+
+fn mod_inverse(a: i64, n: i64) -> i64 {
+    let mut t = 0;
+    let mut newt = 1;
+    let mut r = n;
+    let mut newr = a;
+
+    while newr != 0 {
+        let quotient = r / newr;
+        (t, newt) = (newt, t - quotient * newt);
+        (r, newr) = (newr, r - quotient * newr);
+    }
+    if r > 1 {
+        panic!();
+    }
+    if t < 0 {
+        t = t + n
+    }
+    return t;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,19 @@
-use num_integer::gcd;
-use rand::{Rng, RngExt};
+mod lenstra;
+use std::io::{self, Write};
+
+use crate::lenstra::ecm;
 
 fn main() {
-    println!("Hello, world!");
-}
+    print!("Number to factor: ");
+    io::stdout().flush().unwrap();
 
-fn lenstra(n: i64) -> i64 {
-    let mut rng = rand::rng();
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let n: i64 = input.trim().parse().expect("Please type a valid integer");
 
-    let cap_a: i64 = rng.random::<i64>() % n;
-    let a: i64 = rng.random::<i64>() % n;
-    let b: i64 = rng.random::<i64>() % n;
-
-    let p = (a, b);
-
-    let cap_b = ((b * b) - (a * a * a) - cap_a * a) % n;
-
-    let cap_e = elliptic_curve::new(cap_a, cap_b, n);
-
-    let q1 = p;
-    let mut iter = 0;
-    let mut done = false;
-    while !done {
-        for _ in 0..iter {
-            
-        }
-    }
-
-    return 2;
-}
-
-struct elliptic_curve {
-    a: i64,
-    b: i64,
-    n: i64,
-}
-
-impl elliptic_curve {
-    fn new(a: i64, b: i64, n: i64) -> elliptic_curve {
-        elliptic_curve { a, b, n }
-    }
-
-    fn calculate(&self, x: i64) -> i64 {
-        let y = ((x * x * x) + (self.a * x) + self.b) % self.n;
-        return y;
-    }
-}
-
-fn eliptic_curve(x: i64, a: i64, b: i64, n: i64) -> i64 {
-    let y = ((x * x * x) + (a * x) + b) % n;
-    return y;
-}
-
-fn add_points(p: (i64, i64), q: (i64, i64), a: i64, n: i64) -> Result<(i64, i64), i64> {
-    let mut alpha: i64;
-    if p == q {
-        let nämnare = (2 * p.1) % n;
-        let täljare = (3 * p.0 * p.0 + a) % n;
-        if gcd(nämnare, n) != 1 {
-            return Err(64);
-        } else {
-            alpha = (täljare * mod_inverse(nämnare, n)) % n;
-        }
-        alpha = ((3 * p.0 * p.0 + a) / (2 * p.1)) % n;
-    } else {
-        alpha = ((q.1 - p.1) / (q.1 - p.0)) % n;
-    }
-    let x3 = (alpha * alpha * alpha - p.0 - q.0) % n;
-    let y3 = (alpha * (p.0 - x3) - p.1) % n;
-    return (x3, y3);
-}
-
-fn mod_inverse(a: i64, n: i64) -> Result<i64, String> {
-    let mut t = 0;
-    let mut newt = 1;
-    let mut r = n;
-    let mut newr = a;
-
-    while newr != 0 {
-        let quotient = r / newr;
-        (t, newt) = (newt, t - quotient * newt);
-        (r, newr) = (newr, r - quotient * newr);
-    }
-    if r > 1 {
-        return Err("Icke inverterbar".to_string());
-    }
-    if t < 0 {
-        t = t + n
-    }
-    return Ok(t);
+    println!("Factoring: {}", n);
+    let result = ecm(n);
+    println!("Result: {:?}", result);
 }
